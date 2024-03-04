e3a42240e75526f98646644ee69aa10f7b10127d
Gabriel Manika Koeb <gmkoeb@gmail.com>
2024-03-04 09:14:02 -0300

    Separa tabelas e cria relação entre elas
    Agora o script que popula o banco de dados cria três tabelas,
    patients, doctors e exams. A tabela exams tem duas fk patient_id e
    doctor_id, desse modo ela se relaciona com as outras tabelas do banco.
    A query que seleciona os dados no endpoint /tests foi alterada de acordo
    com essas mudanças

--- import_from_csv.rb
+++ import_from_csv.rb
@@ -1,15 +1,17 @@
 require 'csv'
 require 'pg'
+require_relative 'import_from_csv.rb'
 
 def convert_data
   rows = CSV.read("./data/data.csv", col_sep: ';')
 
   columns = rows.shift
 
-  create_table
+  create_tables
+
   pp 'Data conversion started'
   rows.map do |row|
-    populate_table(row)
+    populate_tables(row)
   end
   pp 'Data conversion ended'
 end
@@ -25,54 +27,115 @@ def db_connection
   end
 end
 
-def create_table
+def create_tables
   db_connection do |connection|
-    connection.exec('CREATE TABLE tests (
-      id SERIAL PRIMARY KEY,
-      registration_number VARCHAR(255),
-      patient_name VARCHAR(255),
-      patient_email VARCHAR(255),
-      patient_birth_date DATE,
-      patient_address VARCHAR(255),
-      patient_city VARCHAR(255),
-      patient_state VARCHAR(255),
-      doctor_crm VARCHAR(255),
-      doctor_crm_state VARCHAR(255),
-      doctor_name VARCHAR(255),
-      doctor_email VARCHAR(255),
-      exam_token VARCHAR(255),
-      exam_date DATE,
-      exam_type VARCHAR(255),
-      exam_type_limits VARCHAR(255),
-      exam_type_result VARCHAR(255)
-    );')
+    connection.exec(
+      'CREATE TABLE patients (
+        id SERIAL PRIMARY KEY,
+        registration_number VARCHAR(255) UNIQUE,
+        name VARCHAR(255),
+        email VARCHAR(255) UNIQUE,
+        birth_date DATE,
+        address VARCHAR(255),
+        city VARCHAR(255),
+        state VARCHAR(255)
+      );')
+    puts "Table 'patients' created successfully"
+
+    connection.exec(
+      'CREATE TABLE doctors (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255),
+        email VARCHAR(255) UNIQUE,
+        crm VARCHAR(255),
+        crm_state VARCHAR(255),
+        UNIQUE (crm, crm_state)
+      );')
+    puts "Table 'doctors' created successfully"
+
+    connection.exec(
+      'CREATE TABLE tests (
+        id SERIAL PRIMARY KEY,
+        patient_id INT REFERENCES patients(id),
+        doctor_id INT REFERENCES doctors(id),
+        token VARCHAR(255),
+        date DATE,
+        type VARCHAR(255),
+        type_limits VARCHAR(255),
+        type_result VARCHAR(255)
+      );')
     puts "Table 'tests' created successfully"
   rescue PG::Error => e
     puts "Error creating table: #{e.message}"
   end
 end
 
-def populate_table(row)
+def populate_tables(row)
   db_connection do |connection|
-    connection.exec('INSERT INTO tests (
-      registration_number,
-      patient_name,
-      patient_email,
-      patient_birth_date,
-      patient_address,
-      patient_city,
-      patient_state,
-      doctor_crm,
-      doctor_crm_state,
-      doctor_name,
-      doctor_email,
-      exam_token,
-      exam_date,
-      exam_type,
-      exam_type_limits,
-      exam_type_result
-    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)',
-    [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15]])
+    patient_id = find_or_create_patient(row)
+    doctor_id = find_or_create_doctor(row)
+    connection.exec(
+      'INSERT INTO tests (
+        patient_id,
+        doctor_id,
+        token,
+        date,
+        type,
+        type_limits,
+        type_result
+      )
+      VALUES ($1, $2, $3, $4, $5, $6, $7)',
+      [patient_id, doctor_id, row[11], row[12], row[13], row[14], row[15]]
+    )
+  end
+end
+
+def find_or_create_patient(row)
+  db_connection do |connection|
+    patient_id_query = connection.exec(
+      'SELECT id FROM patients
+       WHERE registration_number = $1',
+       [row[0]]
+    )
+    if patient_id_query.to_a.empty?
+      patient_id_query = connection.exec(
+        'INSERT INTO patients (
+          registration_number,
+          name,
+          email,
+          birth_date,
+          address,
+          city,
+          state
+        )
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING id',
+        [row[0], row[1], row[2], row[3], row[4], row[5], row[6]])
+    end
+    patient_id_query.first['id']
+  end
+end
+
+def find_or_create_doctor(row)
+  db_connection do |connection|
+    doctor_id_query = connection.exec(
+      'SELECT id FROM doctors
+       WHERE crm = $1',
+       [row[7]]
+      )
+    if doctor_id_query.to_a.empty?
+      doctor_id_query = connection.exec(
+        'INSERT INTO doctors(
+          crm,
+          crm_state,
+          name,
+          email
+          )
+         VALUES ($1, $2, $3, $4)
+         RETURNING id',
+         [row[7], row[8], row[9], row[10]])
+    end
+    doctor_id_query.first['id']
   end
 end
 
--- server.rb
+++ server.rb
@@ -6,7 +6,13 @@ get '/tests' do
   content_type :json
   db_connection do |connection|
     begin
-      tests = connection.exec('SELECT * FROM tests;').to_a
+      tests = connection.exec('SELECT tests.id, patients.name AS patient_name, patients.registration_number, patients.email AS patient_email,
+                               patients.birth_date, patients.address, patients.city, patients.state,
+                               doctors.name AS doctor_name, doctors.email AS doctor_email, doctors.crm, doctors.crm_state,
+                               tests.date, tests.token, tests.type, tests.type_limits, tests.type_result
+                               FROM tests
+                               INNER JOIN patients ON tests.patient_id = patients.id
+                               INNER JOIN doctors ON tests.doctor_id = doctors.id;').to_a
       tests.to_json
     rescue PG::Error => e
       status 500
