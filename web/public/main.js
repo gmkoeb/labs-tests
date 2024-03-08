a5a2d3d57e568cef25729bc8ea420fc933dc3ee6
Gabriel Manika Koeb <gmkoeb@gmail.com>
2024-03-07 21:34:07 -0300

    Adiciona ponto e vírgula

--- web/public/main.js
+++ web/public/main.js
@@ -1,6 +1,6 @@
-const testsUrl = 'http://localhost:3000/tests' + environment()
-const doctorsUrl = 'http://localhost:3000/doctors' + environment()
-const patientsUrl = 'http://localhost:3000/patients' + environment()
+const testsUrl = 'http://localhost:3000/tests' + environment();
+const doctorsUrl = 'http://localhost:3000/doctors' + environment();
+const patientsUrl = 'http://localhost:3000/patients' + environment();
 
 fetch(testsUrl)
   .then((response) => response.json())
@@ -57,112 +57,112 @@ fetch(testsUrl)
 
 
 function environment() {
-  const isUserAgentHeadless = navigator.userAgent.includes('HeadlessChrome')
+  const isUserAgentHeadless = navigator.userAgent.includes('HeadlessChrome');
   if (isUserAgentHeadless === true) {
-    return '?env=test'
+    return '?env=test';
   }else
   {
-    return ''
+    return '';
   }
 }
 
 function toggleElement(event) {
-  const doctors = document.getElementById('doctors')
-  const patients = document.getElementById('patients')
-  const exams = document.getElementById('exams')
+  const doctors = document.getElementById('doctors');
+  const patients = document.getElementById('patients');
+  const exams = document.getElementById('exams');
   const buttons = document.querySelectorAll('.navLink');
-  const clickedButton = event.target
-  clickedButton.classList.toggle('active')
+  const clickedButton = event.target;
+  clickedButton.classList.toggle('active');
   buttons.forEach((btn) => {
     if (btn !== clickedButton) {
       btn.classList.remove('active');
     }
   })
   if(event.target.innerHTML === 'Exames'){
-    doctors.style.display = "none"
-    patients.style.display = "none"
-    exams.style.display = ""
+    doctors.style.display = "none";
+    patients.style.display = "none";
+    exams.style.display = "";
   }else if(event.target.innerHTML === 'Médicos'){
-    getDoctors(doctorsUrl)
-    doctors.style.display = ""
-    patients.style.display = "none"
-    exams.style.display = "none"
+    getDoctors(doctorsUrl);
+    doctors.style.display = "";
+    patients.style.display = "none";
+    exams.style.display = "none";
   }else{
-    getPatients(patientsUrl)
-    doctors.style.display = "none"
-    patients.style.display = ""
-    exams.style.display = "none"
+    getPatients(patientsUrl);
+    doctors.style.display = "none";
+    patients.style.display = "";
+    exams.style.display = "none";
   }
 }
 
 function getDoctors(doctorsUrl){
-  doctorsTable = document.getElementById('doctorsBody')
+  doctorsTable = document.getElementById('doctorsBody');
   if (doctorsTable.childElementCount === 0){
     fetch(doctorsUrl).
     then((response) => response.json()).
     then((data) => {
       data.forEach(function(doctor) {
-        const tr = document.createElement('tr')
-        const doctorName = document.createElement('td')
-        const doctorEmail = document.createElement('td')
-        const crm = document.createElement('td')
-        const crmState = document.createElement('td')
+        const tr = document.createElement('tr');
+        const doctorName = document.createElement('td');
+        const doctorEmail = document.createElement('td');
+        const crm = document.createElement('td');
+        const crmState = document.createElement('td');
 
-        doctorName.textContent = `${doctor.name}`
-        doctorEmail.textContent = `${doctor.email}`
-        crm.textContent = `${doctor.crm}`
-        crmState.textContent = `${doctor.crm_state}`
+        doctorName.textContent = `${doctor.name}`;
+        doctorEmail.textContent = `${doctor.email}`;
+        crm.textContent = `${doctor.crm}`;
+        crmState.textContent = `${doctor.crm_state}`;
 
-        tr.appendChild(doctorName)
-        tr.appendChild(doctorEmail)
-        tr.appendChild(crm)
-        tr.appendChild(crmState)
-        doctorsTable.appendChild(tr)
+        tr.appendChild(doctorName);
+        tr.appendChild(doctorEmail);
+        tr.appendChild(crm);
+        tr.appendChild(crmState);
+        doctorsTable.appendChild(tr);
       })
     }).
     catch(function(error) {
-      console.log(error)
+      console.log(error);
     })
   }
 }
 
 function getPatients(patientsUrl){
-  patientsTable = document.getElementById('patientsBody')
+  patientsTable = document.getElementById('patientsBody');
   if (patientsTable.childElementCount === 0){
     fetch(patientsUrl).
     then((response) => response.json()).
     then((data) => {
       data.forEach(function(patient) {
-        const tr = document.createElement('tr')
-        const patientName = document.createElement('td')
-        const registrationNumber = document.createElement('td')
-        const patientEmail = document.createElement('td')
-        const birthDate = document.createElement('td')
-        const address = document.createElement('td')
-        const city = document.createElement('td')
-        const state = document.createElement('td')
+        const tr = document.createElement('tr');
+        const patientName = document.createElement('td');
+        const registrationNumber = document.createElement('td');
+        const patientEmail = document.createElement('td');
+        const birthDate = document.createElement('td');
+        const address = document.createElement('td');
+        const city = document.createElement('td');
+        const state = document.createElement('td');
 
-        patientName.textContent = `${patient.name}`
-        registrationNumber.textContent = `${patient.registration_number}`
-        patientEmail.textContent = `${patient.email}`
-        birthDate.textContent = `${patient.birth_date}`
-        address.textContent = `${patient.address}`
-        city.textContent = `${patient.city}`
-        state.textContent = `${patient.state}`
+        patientName.textContent = `${patient.name}`;
+        registrationNumber.textContent = `${patient.registration_number}`;
+        patientEmail.textContent = `${patient.email}`;
+        birthDate.textContent = `${patient.birth_date}`;
+        address.textContent = `${patient.address}`;
+        city.textContent = `${patient.city}`;
+        state.textContent = `${patient.state}`;
 
-        tr.appendChild(patientName)
-        tr.appendChild(registrationNumber)
-        tr.appendChild(patientEmail)
-        tr.appendChild(birthDate)
-        tr.appendChild(address)
-        tr.appendChild(city)
-        tr.appendChild(state)
-        patientsTable.appendChild(tr)
+        tr.appendChild(patientName);
+        tr.appendChild(registrationNumber);
+        tr.appendChild(patientEmail);
+        tr.appendChild(birthDate);
+        tr.appendChild(address);
+        tr.appendChild(city);
+        tr.appendChild(state);
+        patientsTable.appendChild(tr);
 
     })
   }).
   catch(function(error) {
-    console.log(error)
+    console.log(error);
   })}
 }
 
