ce0716b8d241d64a5617b9fa6123c298740c3c17
Gabriel Manika Koeb <gmkoeb@gmail.com>
2024-03-08 17:00:40 -0300

    Corrige problema com testes criando dados no banco de development

--- api/spec/requests/tests_api_spec.rb
+++ api/spec/requests/tests_api_spec.rb
@@ -77,32 +77,42 @@ describe 'Tests API' do
 
   context 'GET /tests/:token' do
     it 'success' do
-      uri = URI("http://localhost:3000/tests/IQCZ17")
+      Patient.create(name: 'Paciente de Teste', email: 'email@email.com', registration_number: '123.456',
+                     birth_date: '2022-02-03', address: 'Rua teste', city: 'Cidade teste',
+                     state: 'Estado teste')
+      Doctor.create(name: 'Doutor 1', email: 'doutor1@email.com', crm: 'ABC123', crm_state: 'TE')
+      Doctor.create(name: 'Doutor de Teste', email: 'doutor@email.com', crm: 'DEF456', crm_state: 'TE')
+      Test.create(patient_id: 1, doctor_id: 1, token: 'TOKEN123', date: '2022-01-03', type: 'hemácias',
+                  type_limits: '97-102', type_result: '412')
+      Test.create(patient_id: 1, doctor_id: 1, token: 'Token456', date: '2022-01-03', type: 'hemácias',
+                  type_limits: '97-102', type_result: '412')
+      uri = URI("http://localhost:3000/tests/TOKEN123?env=test")
       response = Net::HTTP.get_response(uri)
       json_response = JSON.parse(response.body)
 
       expect(response.code).to eq '200'
       expect(response.content_type).to include 'application/json'
       expect(json_response.length).to eq 1
-      expect(json_response[0]["token"]).to include('IQCZ17')
-      expect(json_response[0]["registration_number"]).to include('048.973.170-88')
-      expect(json_response[0]["name"]).to include('Emilly Batista Neto')
-      expect(json_response[0]["email"]).to include('gerald.crona@ebert-quigley.com')
-      expect(json_response[0]["birth_date"]).to include('2001-03-11')
-      expect(json_response[0]["doctor"]["crm"]).to include('B000BJ20J4')
-      expect(json_response[0]["doctor"]["crm_state"]).to include('PI')
-      expect(json_response[0]["doctor"]["name"]).to include('Maria Luiza Pires')
-      expect(json_response[0]["date"]).to include('2021-08-05')
+      expect(json_response[0]["token"]).to include('TOKEN123')
+      expect(json_response[0]["registration_number"]).to include('123.456')
+      expect(json_response[0]["name"]).to include('Paciente de Teste')
+      expect(json_response[0]["email"]).to include('email@email.com')
+      expect(json_response[0]["birth_date"]).to include('2022-02-03')
+      expect(json_response[0]["doctor"]["crm"]).to include('ABC123')
+      expect(json_response[0]["doctor"]["crm_state"]).to include('TE')
+      expect(json_response[0]["doctor"]["name"]).to include('Doutor 1')
+      expect(json_response[0]["date"]).to include('2022-01-03')
       expect(json_response[0]["tests"][0]["type"]).to include('hemácias')
-      expect(json_response[0]["tests"][0]["type_limits"]).to include('45-52')
-      expect(json_response[0]["tests"][0]["type_result"]).to include('97')
+      expect(json_response[0]["tests"][0]["type_limits"]).to include('97-102')
+      expect(json_response[0]["tests"][0]["type_result"]).to include('412')
+      expect(json_response[1]).to eq nil
     end
   end
 
   context 'POST /import' do
     it 'success' do
       csv_file_path = '/app/spec/support/csv/test.csv'
-      uri = URI("http://localhost:3000/import")
+      uri = URI("http://localhost:3000/import?env=test")
       request = Net::HTTP::Post.new(uri)
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Post::Multipart.new(uri,
@@ -118,7 +128,7 @@ describe 'Tests API' do
 
     it 'falha caso extensão de arquivo não seja csv' do
       pdf_file_path = '/app/spec/support/pdf/dummy.pdf'
-      uri = URI("http://localhost:3000/import")
+      uri = URI("http://localhost:3000/import?env=test")
       request = Net::HTTP::Post.new(uri)
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Post::Multipart.new(uri,
